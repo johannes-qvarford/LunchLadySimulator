42644ef27825e40bfccf65796ab398626c314642
Jonas Hallin <c11jonha@student.his.se>
2014-04-14 11:17:09 +0200

    Added liquid physics prototype
     assets

---
+++ Assets/Scripts/LightingChange.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class LightingChange : MonoBehaviour
+{
+	
+	public List<Light> Lights;
+
+	
+	void OnPreRender()
+	{
+		foreach (Light light in Lights)
+		{
+			light.enabled = true;
+		}
+	}
+	
+	/*void OnPostRender()
+	{
+		foreach (Light light in Lights)
+		{
+			//light.enabled = true;
+		}
+	}*/
+}
