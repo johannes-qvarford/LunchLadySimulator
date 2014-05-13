e1be0c0ffe08f24c1e19a5adc5aa4026d5164e29
JonasHallin <c11jonha@SE00812.hs.local>
2014-05-13 14:35:43 +0200

    Impatience bas

---
+++ Assets/Impatience.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Impatience : MonoBehaviour {
+	public float[] multipliers;
+	public float[] timeTreshHolds;
+	public TrayBehaviour tray;
+	private int currentLevel;
+	private float startTime;
+	private float currentMultiplier;
+	// Use this for initialization
+	void Start () {
+		startTime = Time.time;
+		currentMultiplier = multipliers [currentLevel];
+		tray.timeMultiplier = currentMultiplier;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(currentLevel >= timeTreshHolds.Length -1)
+		{
+			return;
+			Debug.Log("Totaly pissed off");
+		}
+		if(Time.time - startTime > timeTreshHolds[currentLevel +1])
+		{
+			increaseTreshHold();
+		}
+	}
+	private void increaseTreshHold()
+	{
+		Debug.Log ("Growing impatient.");
+		currentLevel++;
+		currentMultiplier = multipliers [currentLevel];
+		tray.timeMultiplier = currentMultiplier;
+	}
+	public int getImpatienceLevel()
+	{
+		return currentLevel;
+	}
+	public float getCurrentMultiplier()
+	{
+		return currentMultiplier;
+	}
+}
