3b11678b1b1cf314ac6006718382362fd7fe2c92
Persson92 <pirsson_92@hotmail.com>
2014-05-15 13:50:44 +0200

    start of PlateSpawn

---
+++ Assets/SpawnPlateInStack.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpawnPlateInStack : MonoBehaviour {
+
+	public GameObject spawnObject;
+	public int items;
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
