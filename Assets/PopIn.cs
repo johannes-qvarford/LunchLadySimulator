001128a32681ab82cb81b1570b52315d040b50ee
JonasHallin <c11jonha@SE00812.hs.local>
2014-05-20 11:35:15 +0200

    Speech bubble fix

---
+++ Assets/PopIn.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PopIn : MonoBehaviour {
+	public AnimationCurve curve;
+	public float popInSpeed;
+	public GameObject smoke;
+	private float creationTime;
+	// Use this for initialization
+	void Start () {
+		creationTime = Time.time;
+		transform.localScale = new Vector3(0, 0, 0);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float scale = curve.Evaluate ((Time.time - creationTime)*popInSpeed);
+		transform.localScale = new Vector3(scale, scale, scale);
+	}
+}
