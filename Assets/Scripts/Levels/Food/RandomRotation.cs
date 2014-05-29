c9a36131748a4babed20fa49f964067ab8c67236
Persson92 <pirsson_92@hotmail.com>
2014-05-29 15:13:19 +0200

    added random rotation to cookie when they spawn, fixed some positions on items in master scene

---
+++ Assets/Scripts/Levels/Food/RandomRotation.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RandomRotation : MonoBehaviour {
+	Transform start;
+	float yRotate;
+	void Start () 
+	{
+		yRotate = Random.Range(0,360);
+		start = transform;
+		transform.rotation = Quaternion.Euler(start.rotation.x,yRotate,start.rotation.z);
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+	
+	}
+}
